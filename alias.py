a54cdf6f845cf09245b523274d4c5b88d02002d4
Zach <47896974+Zachdehooge@users.noreply.github.com>
2024-07-03 12:34:12 -0400

    Added Alias.py
    Alias.py is a simple python script to automate adding an alias to the .bashrc file

---
+++ alias.py
@@ -0,0 +1,27 @@
+import subprocess
+
+print(r"""
+______________________________________________________________________
+
+A simple python script to automate adding an alias to the .bashrc file
+______________________________________________________________________
+""")
+
+cmd = "grep -R 'bashrc' /home | head -1"
+
+subprocess.run(["grep -R 'bashrc' /home | head -1"], shell=True) 
+
+
+bashrc = input("\nCopy and paste the FULL PATH to the .bashrc file in /home above: ")
+path = input("Enter a path to the program: ")
+alias = input("Enter the alias: ")
+
+
+with open(bashrc, "a") as myfile:
+    myfile.write("alias " + alias + "=" + "'" + path + "'\n")
+
+myfile.close
+
+input("PRESS ANY KEY TO RESTART SHELL")
+
+subprocess.run(["source ~/.bashrc"], shell=True)
